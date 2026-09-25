0736d63f652dfc70839c6661dfddc817f2c1c9a4
alwahibiwafi07 <alwahibiwafi321@gmail.com>
2026-09-25 13:10:25 +0700

    first commit - clean

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # web-data-typescript
+# web-data-typescript
---
+++ index.ts
@@ -0,0 +1,84 @@
+const kontainerData = document.getElementById("kontainerData") as HTMLElement;
+const search = document.getElementById('search') as HTMLInputElement;
+const nilaiRata2 = document.getElementById('nilaiRata2') as HTMLElement;
+
+type siswa = {name: string, class: string, score: number}
+   const siswa: siswa[]  = [
+        { name: "Alwahibi wafi", 
+          class: "A", 
+          score: 100 },
+
+        { name: "Joko",
+          class: "C", 
+          score: 89 },
+
+        { name: "syifa", 
+          class: "B", 
+          score: 80 },
+
+        { name: "marcel", 
+          class: "C", 
+          score: 88 },
+
+        { name: "orinta", 
+          class: "B", 
+          score: 78 },
+
+        { name: "aureliyu", 
+          class: "A", 
+          score: 85 },
+
+        { name: "Rizky", 
+          class: "B", 
+          score: 80 },
+
+        { name: "Prabu",
+          class: "C", 
+          score: 73 },
+
+        { name: "Hafiz", 
+          class: "B",
+          score: 96 },
+
+        { name: "Bahri",
+          class: "E", 
+          score: 0 }
+    ];
+
+function renderData(dataSiswa: siswa[])  {
+    const tampilanData = dataSiswa.map((item, index:number) => `
+        <tr>
+            <td class="judultable">${index + 1}</td>
+            <td class="namasiswa">${item.name}</td>
+            <td class="judultable">${item.class}</td>
+            <td class="judultable">${item.score}</td>
+        </tr>
+    `).join('');
+    
+    kontainerData.innerHTML = tampilanData;
+
+    if (dataSiswa.length > 0) {
+        const totalScore = dataSiswa.reduce((total, item) => total + item.score, 0);
+        const averageScore = totalScore / dataSiswa.length;
+
+        nilaiRata2.innerHTML = `<h4>Rata-rata Nilai: ${averageScore.toFixed(2)}</h4>`;
+    } else {
+        nilaiRata2.innerHTML = `<h4>Rata-rata Nilai: 0</h4>`;
+    }
+}
+
+
+search.addEventListener('input', function(event) {
+   const target = event.target as HTMLInputElement;
+    const kataKunci = target.value.toLowerCase();
+
+    const filteredSiswa = siswa.filter(item => {
+
+        return item.name.toLowerCase().includes(kataKunci);
+    });
+
+    renderData(filteredSiswa);
+
+    
+});
+renderData(siswa);
